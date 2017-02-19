6457566c10f461685fbd1d56e9ff5d9dd5785cf2
jivoi <ekoz@protonmail.com>
2017-02-19 18:07:17 +0300

    useradd on linux

---
+++ tools/useradd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script to add a user to Linux system
+if [ $(id -u) -eq 0 ]; then
+	username="pntest"
+	password="P@ssw0rd123"
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p $pass $username
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system"
+	exit 2
+fi
