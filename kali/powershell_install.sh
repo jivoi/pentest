bc703dfa853a2a3481aab8ff6a52f42d01133a33
Evgenii Kozyrev <ekoz@12go.asia>
2019-01-17 19:42:08 +0300

    powershell on kali

---
+++ kali/powershell_install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+apt update && apt -y install curl gnupg apt-transport-https
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/powershell.list
+apt update
+apt -y install powershell
+pwsh -v
